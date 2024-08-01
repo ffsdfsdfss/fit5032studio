0cfedcc523e7d129312ef64b985f9a05bd74f3c0
ffsdfsdfss <1961464063@qq.com>
2024-08-01 11:09:59 +0800

    Initial commit

---
+++ FIT5032_MyJ5/FIT5032_MyJ5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_MyJ5.MvcApplication" Language="C#" %>
---
+++ FIT5032_Week05C/FIT5032_Week05C/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_Week05C.Startup))]
+namespace FIT5032_Week05C
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
